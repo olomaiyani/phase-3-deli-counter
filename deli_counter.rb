15d7e71a36402870993dae05d959b544f6660f09
Nana <nanaolomaiyani@gmail.com>
2022-10-27 23:11:17 +0300

    commit tree 780de742c9393c96fd69d10250d3902b34e7fd47

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(array)
+    return puts "The line is currently empty." if array.empty?
+    queue = "The line is currently:"
+    array.each_with_index do |customer, index| 
+        queue += " #{index + 1}. #{customer}"
+    end
+    puts queue
+end
+def take_a_number(line, name)
+    line << name 
+    puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+    return puts 'There is nobody waiting to be served!' if line.empty?
+    puts "Currently serving #{line.shift}."
+end
